967d8f9e1af869adb2616f49119a1df7932e7056
Sinan <nanossn1008@gmail.com>
2025-04-15 17:55:23 +0100

    Configure webpack dev and prod mode

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,13 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
